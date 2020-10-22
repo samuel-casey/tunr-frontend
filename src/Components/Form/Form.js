bcf02f8e134b2c8761a3dca7d256a3fbde407c64
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-22 12:17:46 -0400

    created Form component and added css file within form folder

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Form.css';
 
-function Form() {
+function Form(props) {
+	const [formData, setFormData] = useState(props.song);
+
+	const handleChange = (e) => {
+		const key = e.target.name;
+		const value = e.target.value;
+		setFormData({ ...formData, [key]: value });
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault(); // Prevent Form from Refreshing
+		console.log('formdata: ', formData);
+		// props.handleSubmit(formData); // Submit to Parents desired function
+		// props.history.push('/'); //Push back to display page
+	};
+
 	return (
-		<div className='Form'>
-			<h1>Form</h1>
-			<img src='favicon.ico' alt='tunes' />
+		<div className='song-form'>
+			<h3>Add a new song</h3>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					name='title'
+					value={formData.song}
+					placeholder='Title'
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					name='title'
+					value={formData.artist}
+					placeholder='Artist'
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					name='title'
+					value={formData.time}
+					placeholder='Time'
+					onChange={handleChange}
+				/>
+				<input type='submit' value='Add song' />
+			</form>
 		</div>
 	);
 }
