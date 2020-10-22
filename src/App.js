199f95ffc066c76a5ffb72be57f0ea46c20bfab6
Kim Rasmussen <kim@Kims-MacBook-Pro.local>
2020-10-22 11:08:25 -0500

    app imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Route, Link, Switch} from "react-router-dom"
+import Playlist from "./Components/Playlist/Playlist"
+import FavsList	from "./Components/FavsList/FavsList"
+import Form from "./Components/Form/Form"
 
 function App() {
+
+	const url = "http://tunr-backend.herokuapp.com"
+
+	const [list, setList] = React.useState([])
+
+	const emptySong = {
+		artist: "",
+		title: "",
+		length: 0
+	}
+
 	return (
 		<div className='App'>
 			<h1>Tunr</h1>
