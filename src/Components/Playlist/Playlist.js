15c5f27e39a71abf068dbec7f3fb7d202b447a31
Kim Rasmussen <kim@Kims-MacBook-Pro.local>
2020-10-22 10:39:17 -0500

    folder structure

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Playlist() {
+	return (
+		<div className='Playlist'>
+			<h1>Playlist</h1>
+			<img src='favicon.ico' alt='tunes' />
+		</div>
+	);
+}
+
+export default Playlist;
