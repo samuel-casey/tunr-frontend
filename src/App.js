ecd99ea78056240523525a7efbf74923fb296507
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-22 15:15:51 -0400

    Revert "Feature/playlist"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import Playlist from './Components/Playlist/Playlist';
 import './App.css';
 
 function App() {
-	const songs = [
-		{
-			title: 'Hurricane',
-			artist: 'Bob Dylan',
-			time: '4:30',
-		},
-		{
-			title: 'Galway Girl',
-			artist: 'The High Kings',
-			time: '3:00',
-		},
-	];
-
 	return (
 		<div className='App'>
 			<h1>Tunr</h1>
 			<img src='favicon.ico' alt='tunes' />
-			<Playlist songs={songs} />
 		</div>
 	);
 }
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,46 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Playlist.css';
 
-function Playlist(props) {
-	const loaded = props.songs.map((song, index) => {
-		return (
-			<div className='song' key={index}>
-				<div className='song-info'>
-					<span className='song-title'>
-						<b>Title:</b> {song.title}
-					</span>
-					<span className='song-artist'>
-						<b>Artist:</b> {song.artist}
-					</span>
-					<span class='song-time'>
-						<b>Time:</b> {song.time}
-					</span>
-				</div>
-				<div className='btns'>
-					<Link to='/edit'>
-						<button className='btn save-song' onClick={props.handleSave}>
-							{'❤️'}
-						</button>
-					</Link>
-					<button className='btn edit-song' onCLick={props.selectSong}>
-						Edit
-					</button>
-					<button className='btn remove-song' onClick={props.handleDelete}>
-						Delete
-					</button>
-				</div>
-			</div>
-		);
-	});
-
-	const loading = 'Loading...';
-
+function Playlist() {
 	return (
-		<>
-			<h3>My Playlist</h3>
-			{props.songs.length > 0 ? loaded : loading}
-		</>
+		<div className='Playlist'>
+			<h1>Playlist</h1>
+			<img src='favicon.ico' alt='tunes' />
+		</div>
 	);
 }
 
