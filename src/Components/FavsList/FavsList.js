6fb2e665baaa03c4a3d1c892658a4cece1e0ad04
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-22 15:33:06 -0400

    added in Form, Favslist, and Playlist jsx + starter css

--- src/Components/FavsList/FavsList.js
+++ src/Components/FavsList/FavsList.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
 const FavsList = (props) => {
-	const { songs } = props;
+	// const { favs } = props;
 
-	const loaded = () => (
+	const favs = [{ artist: 'Ed Sheeran', title: 'All on Me', time: '3:18' }];
+
+	const loaded = favs.map((fav) => (
 		<div className='FavsList'>
-			{songs.map((song) => (
-				<article>
-					<h3>{song.artist}</h3>
-					<h3>{song.title}</h3>
-					<h3>{song.time}</h3>
-				</article>
-			))}
+			<article>
+				<h3>{fav.artist}</h3>
+				<h3>{fav.title}</h3>
+				<h3>{fav.time}</h3>
+			</article>
 		</div>
-	);
-	return songs.length > 0 ? loaded() : <h1>Loading...</h1>;
+	));
+
+	return favs.length > 0 ? loaded : <h1>Loading...</h1>;
 };
 
 export default FavsList;
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Playlist.css';
 
-function Playlist() {
+function Playlist(props) {
+	const list = [{ artist: 'Ed Sheeran', title: 'All on Me', time: '3:18' }];
+
+	// const loaded = props.list.map((song, index) => {
+	const loaded = list.map((song, index) => {
+		return (
+			<div className='song' key={index}>
+				<div className='song-info'>
+					<span className='song-title'>
+						<b>Title:</b> {song.title}
+					</span>
+					<span className='song-artist'>
+						<b>Artist:</b> {song.artist}
+					</span>
+					<span class='song-time'>
+						<b>Time:</b> {song.time}
+					</span>
+				</div>
+				<div className='btns'>
+					<Link to='/edit'>
+						<button className='btn save-song' onClick={props.handleSave}>
+							{'❤️'}
+						</button>
+					</Link>
+					<button className='btn edit-song' onCLick={props.selectSong}>
+						Edit
+					</button>
+					<button className='btn remove-song' onClick={props.handleDelete}>
+						Delete
+					</button>
+				</div>
+			</div>
+		);
+	});
+
+	const loading = 'Loading...';
+
 	return (
-		<div className='Playlist'>
-			<h1>Playlist</h1>
-			<img src='favicon.ico' alt='tunes' />
-		</div>
+		<>
+			<h3>My Playlist</h3>
+			{/* {props.list.length > 0 ? loaded : loading} */}
+			{list.length > 0 ? loaded : loading}
+		</>
 	);
 }
 
