d61282ac7b2f8132b21d57c1668479b183803e42
jb0nd87 <68964150+jb0nd87@users.noreply.github.com>
2020-10-22 12:09:54 -0700

    favelist component created

--- src/Components/FavsList/FavsList.js
+++ src/Components/FavsList/FavsList.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
-function FavsList() {
-	return (
+const FavsList = (props) => {
+	const { songs } = props;
+
+	const loaded = () => (
 		<div className='FavsList'>
-			<h1>Favorites List</h1>
-			<img src='favicon.ico' alt='tunes' />
+			{songs.map((song) => (
+				<article>
+					<h3>{song.artist}</h3>
+					<h3>{song.title}</h3>
+					<h3>{song.time}</h3>
+					<button
+						onClick={() => {
+							props.selectSong(song);
+							props.history.push('/edit');
+						}}>
+						Edit
+					</button>
+					<button
+						onClick={() => {
+							props.deleteSong(song);
+						}}>
+						Delete
+					</button>
+				</article>
+			))}
 		</div>
 	);
-}
+	return songs.length > 0 ? loaded() : <h1>Loading...</h1>;
+};
 
 export default FavsList;
