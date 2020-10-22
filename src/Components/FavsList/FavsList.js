8084728fd9a3a87118ced860d8232c920a649a49
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-22 15:41:35 -0400

    updated songs prop in favs list to favs

--- src/Components/FavsList/FavsList.js
+++ src/Components/FavsList/FavsList.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './FavsList.css';
 
 function FavsList(props) {
-	const loaded = props.songs.map((song, index) => {
+	const loaded = props.favs.map((fav, index) => {
 		return (
-			<div className='song' key={index}>
+			<div className='fav' key={index}>
 				<div>
-					<span className='song-title'>
-						<b>Title:</b> {song.title}
+					<span className='fav-title'>
+						<b>Title:</b> {fav.title}
 					</span>
-					<span className='song-artist'>
-						<b>Artist:</b> {song.artist}
+					<span className='fav-artist'>
+						<b>Artist:</b> {fav.artist}
 					</span>
-					<span className='song-time'>
-						<b>Time:</b> {song.time}
+					<span className='fav-time'>
+						<b>Time:</b> {fav.time}
 					</span>
 				</div>
 			</div>
@@ -25,9 +25,8 @@ const loading = 'Loading...';
 return (
 	<>
 		<h3>My Fave List</h3>
-		{props.songs.length > 0 ? loaded : loading}
+		{props.favs.length > 0 ? loaded : loading}
 	</>
 );
-
 
 export default FavsList;
